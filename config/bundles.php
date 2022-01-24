da352f9700774504993e44c14c7ebe82fce0bfde
lecomte38 <nicolas.lecomte@etu.unilim.fr>
2022-01-24 20:19:23 +0000

    UE 235 - mini-projet

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
